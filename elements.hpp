dfd968deae0162a13d05e7e7c4a1d28c055bf6bc
Arturo Ruiz Mañas <arruma2160@gmail.com>
2018-04-14 17:26:04 +0200

    Node and test

--- elements.hpp
+++ elements.hpp
@@ -1,4 +1,6 @@
-#include <queue>
+#include <vector>
+#include <algorithm>
+using std::vector;
 
 class Edge 
 {
@@ -7,21 +9,65 @@ class Edge
             :mFromNode{fromNode}, mToNode{toNode}, mDistance{distance} 
         {}
         
-        virtual int fromNode(void){return mFromNode;}
-        virtual int toNode(void){return mToNode;}
-        virtual int distance(void){return mDistance;}
+        // Getters
+        virtual int fromNode(void) const {return mFromNode;}
+        virtual int toNode(void) const {return mToNode;}
+        virtual int distance(void) const {return mDistance;}
 
+        // Setters
         virtual void set_distance(int distance){mDistance = distance;}
+        virtual void set_from_node(int fromNode) = delete;
+        virtual void set_to_node(int toNode) = delete;
 
         friend bool operator< (const Edge& lhs, const Edge& rhs) {
             // Definition of less than relation between two edges:
-            // Edge1 < Edge2 if Edge1.distance > Edge2.distance
+            // Priority => Edge1 < Edge2 if Edge1.distance > Edge2.distance
             return lhs.mDistance > rhs.mDistance;
+        }
+
+        friend bool operator> (const Edge& lhs, const Edge& rhs) {
+            return lhs.mDistance < rhs.mDistance;
         }
 
     private:
         int mFromNode;
         int mToNode;
         int mDistance;
+};
+
+
+class Node
+{
+    struct greater 
+    {
+        bool operator()(const Edge& lhs, const Edge& rhs) {
+            return lhs > rhs ;
+        }
+    };
+    public:
+        Node(int id, vector<int> all_edges_distances)
+            : mId{id}, mTentativeDistance{std::numeric_limits<int>::max()}
+        {
+            int toNode = 0;
+            for(const auto& distance : all_edges_distances)
+            {
+                mEdges.push_back({mId, toNode++, distance});
+            }
+            std::sort(mEdges.begin(), mEdges.end(), greater());
+        }
+
+        // Getters
+        virtual int id(void){return mId;}
+        virtual int tentative_distance(void){return mTentativeDistance;}
+        virtual vector<Edge> all_edges(void){return mEdges;}
+
+        // Setters
+        virtual void set_tentative_distance(int dist){mTentativeDistance = dist;}
+
+    private:
+        const int mId;
+        int mTentativeDistance;
+
+        vector<Edge> mEdges;
 };
 
--- test/test_global.cpp
+++ test/test_global.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include "elements.hpp"
 #include <queue>
+#include <vector>
 
 TEST(Edge, 1_Edge_creation)
 {
@@ -61,6 +62,36 @@ TEST(Edge, 4_Sorting_priority_priority_queue)
     top = pq.top();
     ASSERT_EQ(top.distance(), 90);
     pq.pop();
+}
+
+
+TEST(Node, 1_Node_s_edges_in_order_1)
+{
+    std::vector<int> distances {1,2,3,4,5};
+    Node node {0, distances };
+
+    std::vector<Edge> edges = node.all_edges();
+
+    int i = 0;
+    for(const auto& edge : edges)
+    {
+        ASSERT_EQ(edge.distance(), distances[i++]);
+    }
+}
+
+TEST(Node, 2_Node_s_edges_in_order_2)
+{
+    std::vector<int> distances {5,4,3,2,1};
+    std::vector<int> test_order {1,2,3,4,5};
+
+    Node node {0, distances };
+    std::vector<Edge> edges = node.all_edges();
+
+    int i = 0;
+    for(const auto& edge : edges)
+    {
+        ASSERT_EQ(edge.distance(), test_order[i++]);
+    }
 }
 
 // ---------------------------------
